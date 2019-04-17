8763b733786f995776eee8e44d48b6f334750dda
Evelyn Mendes <evlymn@gmail.com>
2019-04-17 14:29:01 -0300

    fontes main

---
+++ public/scripts/api-3/render-footer.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", async function() {
+  const user = await firebase
+    .auth()
+    .signInWithEmailAndPassword("evelyn@devas.io", "12345678");
+
+  if (user) {
+    const idToken = await firebase.auth().currentUser.getIdToken();
+    const xhttpFooter2 = new XMLHttpRequest();
+    xhttpFooter2.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("componente-footer").innerHTML =
+          xhttpFooter2.responseText;
+      }
+    };
+    xhttpFooter2.open("GET", "https://api3.devas.io/renderfooter", true);
+    xhttpFooter2.setRequestHeader("idToken", idToken);
+    xhttpFooter2.send();
+  }
+});
